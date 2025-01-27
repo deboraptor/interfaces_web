5b74bbd9bb129a2190432cb78437c1171bda04b2
Débora <vdzdebora@gmail.com>
2025-01-27 17:45:08 +0100

    :see_no_evil: suppression des preuves pour le token

--- api_rest/config.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Config:
-    ACCESS_TOKEN: str = "SfqVjLfEgsgqF5qArwKkCO2gjxFqd4BtAsZMsHRyPhXWiRwJuXK0oLb6W2Y1"
-    ZENODO_API = "https://zenodo.org/api/deposit/depositions"
